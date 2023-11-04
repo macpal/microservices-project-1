4a2f37cb4a5520f4baa9876f6a9809f5fb2cc6f8
mayank.pal <mayank.pal@ge.com>
2023-11-04 14:12:16 +0530

    new changes

--- RatingService/RatingService/src/main/java/com/lcwd/rating/RatingServiceApplication.java
+++ RatingService/RatingService/src/main/java/com/lcwd/rating/RatingServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
